a0fa3e4d8fd8411f3cacac8facf8193e61b47b22
Krystle Lee <l33.krys@gmail.com>
2023-09-25 14:13:20 -0700

    set up exercise.py basic functions

---
+++ lib/seed.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from models.__init__ import CONN, CURSOR
+from models.exercise import Exercise
+from models.log import Log
+from models.user import User
+
+def seed_database():
+
+    # Delete existing tables
+    Exercise.drop_table()
+    Log.drop_table()
+    User.drop_table()
+
+    # Create new tables
+    Exercise.create_table()
+    Log.create_table()
+    User.create_table()
+
+    # Create seed data
+    treadmill = Exercise.create("Treadmill", 15, "Cardio", "Advanced", 200)
+    weights = Exercise.create("Weights", 30, "Strength", "Intermediate", 150)
+    walking = Exercise.create("Walking", 30, "Cardio", "Beginner", 100)
+    jump_rope = Exercise.create("Jump Rope", 5, "Cardio", "Beginner", 100)
+    push_ups = Exercise.create("Push Ups", 5, "Strength", "Beginner", 25)
+
+seed_database()
+print("Seeded database")
