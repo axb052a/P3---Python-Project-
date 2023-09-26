f3b700cff2e6ac64cb5d20e54d82908bf01c7fd2
axb052a <119557887+axb052a@users.noreply.github.com>
2023-09-26 11:32:50 -0700

    Update cli.py

--- lib/cli.py
+++ lib/cli.py
@@ -2,7 +2,11 @@
 
 from helpers import (
     exit_program,
-    helper_1
+    helper_1,
+    create_exercise,
+    list_exercises,
+    create_logs,
+    list_logs,
 )
 
 
@@ -14,6 +18,15 @@ def main():
             exit_program()
         elif choice == "1":
             helper_1()
+        elif choice == "2":
+            create_exercise()
+        elif choice == "3":
+            print("Exercise List:")
+            list_exercises()
+        elif choice == "4":
+            create_logs()
+        elif choice == "5":
+            list_logs()
         else:
             print("Invalid choice")
 
@@ -22,7 +35,28 @@ def menu():
     print("Please select an option:")
     print("0. Exit the program")
     print("1. Some useful function")
+    print("2. Create exercise")
+    print("3. Show Exercises")
+    print("4. Create Log") 
+    print("5. Show Logs")
+
+def welcome_menu():
+    print("User Login")
+    print("New User")
+    print("Exit App")
+    
+def log_menu():
+    print("Please select an option:")
+    print("0. Exit the program")
+    print("1. Create Log")
+    print("2. Show Log")
+    print("3. Update a Log")
+    print("4. Delete a Log")
 
 
 if __name__ == "__main__":
+    print("Welcome to PyFit! Track your fitness journey to help you reach your goals.")
     main()
+
+
+
