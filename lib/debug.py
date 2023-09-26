2f047f4115a38e6453ff8b217f290640f78642d9
axb052a <119557887+axb052a@users.noreply.github.com>
2023-09-26 11:33:09 -0700

    Update debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -2,7 +2,35 @@
 # lib/debug.py
 
 from models.__init__ import CONN, CURSOR
+from models.exercise import Exercise
+from models.log import Log
+from models.user import User
 import ipdb
 
+def reset_database():
 
+    # Delete existing tables
+    Exercise.drop_table()
+    Log.drop_table()
+    User.drop_table()
+
+    # Create new tables
+    Exercise.create_table()
+    Log.create_table()
+    User.create_table()
+
+    # Create seed data
+    treadmill = Exercise.create("Treadmill", 15, "Cardio", "Advanced", 200)
+    weights = Exercise.create("Weights", 30, "Strength", "Intermediate", 150)
+    walking = Exercise.create("Walking", 30, "Cardio", "Beginner", 100)
+    jump_rope = Exercise.create("Jump Rope", 5, "Cardio", "Beginner", 100)
+    push_ups = Exercise.create("Push Ups", 5, "Strength", "Beginner", 25)
+   
+    Log.create("John Doe", treadmill, "2023-09-28")
+    Log.create("Alice Smith", weights, "2023-09-28")
+    Log.create("Bob Johnson", walking, "2023-09-27")
+    Log.create("Eve Adams", jump_rope, "2023-09-27")
+    Log.create("Charlie Brown", push_ups, "2023-09-26")
+    
+reset_database()
 ipdb.set_trace()
