85b910c84befeaa1bf656a5137d460003914b806
Krystle Lee <l33.krys@gmail.com>
2023-09-26 09:16:36 -0700

    more progress

--- lib/helpers.py
+++ lib/helpers.py
@@ -18,6 +18,19 @@ def create_exercise():
     except Exception as exc:
         print("Error creating new exercise: ", exc)
 
+# def create_exercise():
+#     name = input("Exercise: ")
+#     time = input("Duration (in minutes): ")
+#     category = input("Category (Cardio or Strength): ")
+#     intensity = input("Intensity (Beginner, Intermediate, Advanced): ")
+#     cals_burned = input("Calories Burned: ")
+#     try:
+#         Exercise.create_table()
+#         exercise = Exercise.create(name, time, category, intensity, cals_burned)
+#         print(f"Success: {exercise} is now available!")
+#     except Exception as exc:
+#         print("Error creating new exercise: ", exc)
+
 def list_exercises():
     exercises = Exercise.get_all()
     for exercise in exercises:
