10617f30e48308e1375bc17484c6e0bea0fd2d14
axb052a <119557887+axb052a@users.noreply.github.com>
2023-09-27 11:47:16 -0700

    Update helpers.py

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,6 +1,7 @@
 # lib/helpers.py
 
 from models.exercise import Exercise
+from models.log import Log
 
 def helper_1():
     print("Performing useful function#1.")
@@ -51,7 +52,7 @@ def update_exercise_by_name_or_id():
             exercise = Exercise.find_by_name(name.title())
             collect_updates(exercise)
         except Exception as exc:
-            print("Error updating exercise")
+            print("Error updating exercise", exc)
 
 def collect_updates(exercise):
         # Update exercise instance
@@ -92,6 +93,84 @@ def delete_exercise_by_name_or_id():
             print(f"{exercise.name} has been deleted") if exercise else print(f"Exercise {exercise.name} not found")
         except Exception as exc:
             print("Error deleting exercise")
+            
+def find_most_and_least_popular_exercises():
+    # Get a list of all exercises
+    all_exercises = Exercise.get_all()
+
+    # Initialize dictionaries to store exercise popularity counts
+    exercise_popularity = {}
+    
+    # Count the popularity of each exercise based on log entries
+    for exercise in all_exercises:
+        log_entries = Log.find_by_exercise(exercise)
+        exercise_popularity[exercise.name] = len(log_entries)
+
+    # Find the most and least popular exercises
+    most_popular_exercise = max(exercise_popularity, key=exercise_popularity.get)
+    least_popular_exercise = min(exercise_popularity, key=exercise_popularity.get)
+
+    print(f"The most popular exercise is: {most_popular_exercise} with {exercise_popularity[most_popular_exercise]} log entries.")
+    print(f"The least popular exercise is: {least_popular_exercise} with {exercise_popularity[least_popular_exercise]} log entries.")
+    
+def create_log():
+    try:
+        user = input("User ID: ")
+        exercise = input("Exercise ID: ")
+        date = input("Date (YYYY-MM-DD): ")
+
+        log = Log.create(user, exercise, date)
+        print("Log created successfully.")
+    except Exception as exc:
+        print("Error creating new log: ", exc)
+
+def list_logs():
+    logs = Log.get_all()
+    for log in logs:
+        print(log)
+
+def get_log_by_id():
+    log_id = input("Log ID: ")
+    try:
+        id_ = int(log_id)
+        log = Log.find_by_id(id_)
+        print(log) if log else print(f"Log with ID {log_id} not found")
+    except:
+        print(f"Invalid Log ID: {log_id}")
+
+def update_log_by_id():
+    log_id = input("Log ID: ")
+    try:
+        id_ = int(log_id)
+        log = Log.find_by_id(id_)
+        if log:
+            collect_log_updates(log)
+        else:
+            print(f"Log with ID {log_id} not found")
+    except:
+        print(f"Invalid Log ID: {log_id}")
+
+def collect_log_updates(log):
+    try:
+        date = input(f"New date for Log ID {log.id} (YYYY-MM-DD): ")
+        log.date = date
+        log.update()
+        print(f"Log entry updated successfully for Log ID {log.id}.")
+    except Exception as exc:
+        print(f"Error updating log entry: {exc}")
+
+def delete_log_by_id():
+    log_id = input("Log ID: ")
+    try:
+        id_ = int(log_id)
+        log = Log.find_by_id(id_)
+        if log:
+            Log.delete(log)
+            print(f"Log entry with ID {log.id} deleted successfully.")
+        else:
+            print(f"Log with ID {log_id} not found")
+    except:
+        print(f"Invalid Log ID: {log_id}")
 
 
 def exit_program():
