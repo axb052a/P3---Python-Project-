f5b8f1bf475cff7442688bbabb40920a873112c2
axb052a <119557887+axb052a@users.noreply.github.com>
2023-09-27 11:12:19 -0700

    Update debug.py
    User and Log

--- lib/debug.py
+++ lib/debug.py
@@ -5,7 +5,7 @@ from models.__init__ import CONN, CURSOR
 from models.exercise import Exercise
 from models.log import Log
 from models.user import User
-import ipdb
+# import ipdb
 
 def reset_database():
 
@@ -19,18 +19,24 @@ def reset_database():
     Log.create_table()
     User.create_table()
 
-    # Create seed data
+  # Create seed data for exercises
     treadmill = Exercise.create("Treadmill", 15, "Cardio", "Advanced", 200)
     weights = Exercise.create("Weights", 30, "Strength", "Intermediate", 150)
     walking = Exercise.create("Walking", 30, "Cardio", "Beginner", 100)
     jump_rope = Exercise.create("Jump Rope", 5, "Cardio", "Beginner", 100)
     push_ups = Exercise.create("Push Ups", 5, "Strength", "Beginner", 25)
-   
-    Log.create("John Doe", treadmill, "2023-09-28")
-    Log.create("Alice Smith", weights, "2023-09-28")
-    Log.create("Bob Johnson", walking, "2023-09-27")
-    Log.create("Eve Adams", jump_rope, "2023-09-27")
-    Log.create("Charlie Brown", push_ups, "2023-09-26")
-    
+
+    # Create seed data for users
+    user1 = User.create("User1", 5, 10, 150)
+    user2 = User.create("User2", 6, 2, 180)
+    user3 = User.create("User3", 5, 8, 160)
+
+   # Create seed data for logs
+    log1 = Log.create(user3, treadmill, "2023-09-27")
+    log2 = Log.create(user2, weights, "2023-09-28")
+    log3 = Log.create(user3, walking, "2023-09-29")
+    log4 = Log.create(user1, jump_rope, "2023-09-29")
+    log5 = Log.create(user2, push_ups, "2023-09-25")
+
 reset_database()
-ipdb.set_trace()
+# ipdb.set_trace()
