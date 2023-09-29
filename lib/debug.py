3272dffb6cf8c4a999adda98bb37cb0f080e6405
axb052a <119557887+axb052a@users.noreply.github.com>
2023-09-29 12:43:16 -0700

    Update debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -5,38 +5,50 @@ from models.__init__ import CONN, CURSOR
 from models.exercise import Exercise
 from models.log import Log
 from models.user import User
-# import ipdb
+import ipdb
 
 def reset_database():
 
-    # Delete existing tables
+     # Delete existing tables
     Exercise.drop_table()
     Log.drop_table()
     User.drop_table()
-
     # Create new tables
     Exercise.create_table()
     Log.create_table()
     User.create_table()
-
-  # Create seed data for exercises
+    # Create seed data
+    
+    # Exercise instances
     treadmill = Exercise.create("Treadmill", 15, "Cardio", "Advanced", 200)
     weights = Exercise.create("Weights", 30, "Strength", "Intermediate", 150)
     walking = Exercise.create("Walking", 30, "Cardio", "Beginner", 100)
     jump_rope = Exercise.create("Jump Rope", 5, "Cardio", "Beginner", 100)
     push_ups = Exercise.create("Push Ups", 5, "Strength", "Beginner", 25)
-
-    # Create seed data for users
-    user1 = User.create("User1", 5, 10, 150)
-    user2 = User.create("User2", 6, 2, 180)
-    user3 = User.create("User3", 5, 8, 160)
-
-   # Create seed data for logs
-    log1 = Log.create(user3, treadmill, "2023-09-27")
-    log2 = Log.create(user2, weights, "2023-09-28")
-    log3 = Log.create(user3, walking, "2023-09-29")
-    log4 = Log.create(user1, jump_rope, "2023-09-29")
-    log5 = Log.create(user2, push_ups, "2023-09-25")
+   
+    # User instances
+    john = User.create_user("John Cena", 6, 1, 200)
+    alicia = User.create_user("Alicia Keys", 5, 6, 120)
+    bob = User.create_user("Bob Mackey", 6, 1, 175)
+    adam = User.create_user("Adam Brody", 5, 11, 170)
+    charlie = User.create_user("Charlie Brown", 4, 2, 75)
+    
+    # Log instances
+    Log.create(john, treadmill, "2023-09-28")
+    Log.create(alicia, weights, "2023-09-29")  
+    Log.create(bob, walking, "2023-09-30")    
+    Log.create(adam, jump_rope, "2023-10-01") 
+    Log.create(charlie, push_ups, "2023-10-02")
+    Log.create(adam, jump_rope, "2023-10-03")
+    Log.create(john, jump_rope, "2023-10-04") 
+    Log.create(adam, jump_rope, "2023-10-05")
+    Log.create(john, jump_rope, "2023-10-06")
+    Log.create(adam, jump_rope, "2023-10-07")
+    Log.create(charlie, push_ups, "2023-10-03")  
+    Log.create(bob, push_ups, "2023-10-04")
+    Log.create(charlie, push_ups, "2023-10-05")
+    Log.create(alicia, push_ups, "2023-10-06")
+    Log.create(charlie, push_ups, "2023-10-07")
 
 reset_database()
-# ipdb.set_trace()
+ipdb.set_trace()
