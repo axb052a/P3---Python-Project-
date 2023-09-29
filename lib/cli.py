a4ed96fe47be490586b5428fde7c39b326b40029
axb052a <119557887+axb052a@users.noreply.github.com>
2023-09-29 12:42:58 -0700

    Update cli.py

--- lib/cli.py
+++ lib/cli.py
@@ -1,142 +1,168 @@
 # lib/cli.py
-
 from models.exercise import Exercise
 from models.log import Log
-
 from helpers import (
     exit_program,
-    helper_1,
     create_exercise,
     list_exercises,
     get_exercise_by_name_or_id,
     update_exercise_by_name_or_id,
-    delete_exercise_by_name_or_id,
-    find_most_and_least_popular_exercises,
+    # delete_exercise_by_name_or_id,
+    delete_exercise_with_logs,
     create_log, 
     list_logs,
     get_log_by_id,
     update_log_by_id,
     delete_log_by_id,
+    show_popular,
+    least_popular,
+    list_users,
+    # create_user,
+    login_user,
+    # get_user_logs,
+    get_user_recent_workout,
+    get_user_workout_history,
+    update_user_info_and_logs,
+    delete_user_with_logs,
+    get_my_info
 )
-
 
 def main():
     home_menu()
     while True:
-        choice = input("> ")
+        choice = input("\033[33m> \033[0m")
         if choice == "x":
             exit_program()
         elif choice == "1":
-            user_menu()
+            try:
+                login_user()
+                user_menu()
+            except:
+                print("\033[31mLogin error. Please try again.\n[x] Return to menu\033[0m")
+        
             while True:
-                choice = input("> ")
+                choice = input("\033[33m> \033[0m")
                 if choice == "1":
-                    print("My Info")
+                    get_my_info()
                 elif choice == "2":
-                    print("My Stats")
+                    print("\033[36mMy Stats\033[0m")
+                    get_user_recent_workout() 
                 elif choice == "3":
-                    print("Workout History")
-                elif choice == "x":
+                    get_user_workout_history()
+                elif choice == "4":
+                    print("\033[36mAll Users\033[0m")
+                    list_users()
+                elif choice == "5":
+                    update_user_info_and_logs()
+                elif choice == "6":
+                    delete_user_with_logs()
+                elif choice == "0" or "x":
                     main()
                 else:
-                    print("Invalid choice user_menu")
+                    print("\033[31mInvalid choice user_menu\033[0m")
         elif choice == "2":
-            print("New User")
-        elif choice == "3":
             exercise_menu()
             while True:
-                choice = input("> ")
+                choice = input("\033[33m> \033[0m")
                 if choice == "1":
                     list_exercises()
                 elif choice == "2":
-                    print("Exercise Stats")
+                    print("\033[36mExercise Stats\033[0m")
+                    show_popular()
+                    least_popular()
                 elif choice == "3":
                     get_exercise_by_name_or_id()
                 elif choice == "4":
                     update_exercise_by_name_or_id()                    
                 elif choice == "5":
-                    delete_exercise_by_name_or_id()
+                    # delete_exercise_by_name_or_id()
+                    delete_exercise_with_logs()
                 elif choice == "6":
                     create_exercise()
-                elif choice == "7":
-                    find_most_and_least_popular_exercises()
                 elif choice == "x":
                     main()
                 else:
-                    print("Invalid choice exercise_menu")
-        elif choice == "4":
+                    print("\033[31mInvalid choice exercise_menu\033[0m")
+        elif choice == "3":
             log_menu()  
             while True:
-                choice = input("> ")
+                choice = input("\033[33m> \033[0m")
                 if choice == "1":
                     list_logs()
                 elif choice == "2":
-                    print("Log Entries")
+                    # log_id = input("Enter the log ID: ")
+                    # get_log_by_id(log_id) 
+                    get_log_by_id() 
                 elif choice == "3":
-                    log_id = input("Enter the log ID: ")
-                    get_log_by_id(log_id) 
-                elif choice == "4":
                     update_log_by_id() 
-                elif choice == "5":
+                elif choice == "4":
                     delete_log_by_id()      
-                elif choice == "6":
+                elif choice == "5":
                     create_log()
                 elif choice == "x":
                     main()
                 else:
-                    print("Invalid choice log_menu")
+                    print("\033[31mInvalid choice log_menu\033[0m")
         else:
-            print("Invalid choice")
+            print("\033[31mInvalid choice\033[0m")
 
 def home_menu():
-    print("Please select an option:")
-    print("[1] User Login")
-    print("[2] New User")
-    print("[3] Exercises")
-    print("[4] Logs")
-    print("[x] Exit App")
+    print("\033[36mPlease select an option:\033[0m")
+    print("\033[36m[1] Users\033[0m")
+    print("\033[36m[2] Exercises\033[0m")
+    print("\033[36m[3] Logs\033[0m")
+    print("\033[36m[x] Exit App\033[0m")
 
 def user_menu():
-    print("Please select an option:")
-    print("[1] My Info")
-    print("[2] My Stats")
-    print("[3] Workout History")
-    print("[x] Log Out")
+    print("\033[36mPlease select an option:\033[0m")
+    print("\033[36m[1] My Info\033[0m")
+    print("\033[36m[2] My Stats\033[0m")
+    print("\033[36m[3] Workout History\033[0m")
+    print("\033[36m[4] All Users\033[0m")
+    print("\033[36m[5] Update an User\033[0m")
+    print("\033[36m[6] Delete an User\033[0m")
+    print("\033[36m[x] Log Out\033[0m")
+
 
 def exercise_menu():
-    print("Please select an option:")
-    print("[1] Exercises")
-    print("[2] Exercise Stats")
-    print("[3] Search for Exercise")
-    print("[4] Update Exercise")  
-    print("[5] Delete Exercise")      
-    print("[6] Create Exercise")
-    print("[7] Find Most and Least Popular Exercises")
-    print("[x] Return to Home")
-
-def exercise_search_menu():
-    print("Please select an option:")
-    print("[1] ID")
-    print("[2] Name")
-    print("[3] Time")
-    print("[4] Category")
-    print("[5] Intensity")
-    print("[6] Calories Burned")
-    print("[x] Return to Exercise Menu")
+    print("\033[36mPlease select an option:\033[0m")
+    print("\033[36m[1] Exercises\033[0m")
+    print("\033[36m[2] Exercise Stats\033[0m")
+    print("\033[36m[3] Search for Exercise\033[0m")
+    print("\033[36m[4] Update Exercise\033[0m")  
+    print("\033[36m[5] Delete Exercise\033[0m")      
+    print("\033[36m[6] Create Exercise\033[0m")
+    print("\033[36m[x] Return to Home\033[0m")
     
+
 def log_menu():
-    print("Please select an option:")
-    print("[1] Log History")
-    print("[2] Log Entries")
-    print("[3] Search for Log")
-    print("[4] Update Log")  
-    print("[5] Delete Log")      
-    print("[6] Create Log")
-    print("[x] Return to Home")
+    print("\033[36mPlease select an option:\033[0m")
+    print("\033[36m[1] Log History\033[0m")
+    print("\033[36m[2] Search for Log\033[0m")
+    print("\033[36m[3] Update Log\033[0m")  
+    print("\033[36m[4] Delete Log\033[0m")      
+    print("\033[36m[5] Create Log\033[0m")
+    print("\033[36m[x] Return to Home\033[0m")
+
+runner = """
+                         .7Y5Y7.       
+                       :#@@@@@#^      
+                       ?@@@@@@@7      
+                 ^J5GB#&@@@@&B?       
+                :&@@#@@@@@B:.         
+                G@@55@@@@@B^^~^       
+               .G#GG@@P?@@@@@@@~      
+                 !B@@@! !?77!!^       
+               .Y@@&&@@P^             
+     :5P5YJ7!^!B@@P::Y&@@5:           
+     ?@@&&@@@@@@&7    :5@@&7          
+      ^^..:^~!77:       ~&@@J         
+                         ^B@@G?.      
+                          .P@@&:      
+                            ~7:  
+                            """
 
 if __name__ == "__main__":
-    print("Welcome to PyFit! Track your fitness journey to help you reach your goals.")
+    print("\033[37mWelcome to PyFit! Track your fitness journey to help you reach your goals.\033[0m")
+    print(runner)
     main()
-
-
-
